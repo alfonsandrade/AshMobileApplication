9a41574f084354de99d987496f760fb6e68dbbad
alfonsandrade <alfons@alunos.utfpr.edu.br>
2024-04-24 22:32:24 -0300

    ASH-46: Start the app project and repository - Have the project on github for version control

---
+++ app/src/main/java/com/example/ashmobileapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ashmobileapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.ashmobileapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
